6a189209faa3d2e0b1aeea882f366302016289d9
devopseasylearning <info@devopseasylearning.com>
2023-01-30 05:07:59 -0600

    test

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-052efd3df9dad4825"
-#   instance_type = "t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name = "web02"
-#   }
-# }
+  tags = {
+    Name = "web02"
+  }
+}
