0d5d01f2ad178e032d29c7afe780b18eed40a3b6
devopseasylearning <info@devopseasylearning.com>
2023-01-31 15:58:41 -0600

    test

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "remote" {
-    organization = "LEARNING01-ORG"
+# terraform {
+#   backend "remote" {
+#     organization = "LEARNING01-ORG"
 
-    workspaces {
-      name = "dev"
-    }
-  }
-}
+#     workspaces {
+#       name = "dev"
+#     }
+#   }
+# }
 
 provider "aws" {
   region     = "us-east-1"
@@ -14,10 +14,28 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-# resource "aws_instance" "app_server1" {
-#   ami           = var.ami
-#   instance_type = var.instance_type
-#   tags = {
-#     Name = "web-server-0"
-#   }
-# }
+resource "aws_instance" "app_server1" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "web-server-0"
+  }
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["user1", "user2", "user3"]
+}
+
+resource "aws_iam_user" "user1" {
+  name = "user1"
+}
+
+resource "aws_iam_user" "user2" {
+  name = "user2"
+}
+
+resource "aws_iam_user" "user3" {
+  name = "user3"
+}
