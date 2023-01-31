9598f42e8ce2f6c332ddda046abd76256343f217
devopseasylearning <info@devopseasylearning.com>
2023-01-31 15:17:01 -0600

    test

---
+++ local-apply/variable.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
